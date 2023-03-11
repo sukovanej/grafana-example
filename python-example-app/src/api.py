bcc1c9496b597d948b1e8d9774b53f496ee756d2
Milan Suk <Milansuk@email.cz>
2023-03-11 19:51:29 +0100

    chore: init python-example-app

---
+++ python-example-app/src/api.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from prometheus_client import Counter, generate_latest
+
+app = FastAPI()
+
+my_endpoint_counter = Counter(
+    "my_enpoint_triggered_count",
+    "Count the number of times /my-endpoint has been triggered",
+    labelnames=["app"],
+    _labelvalues=["python-example-app"],
+)
+
+
+@app.get("/metrics")
+def metrics():
+    return PlainTextResponse(generate_latest())
+
+
+@app.get("/my-endpoint")
+def my_endpoint():
+    my_endpoint_counter.inc()
+    return "zdar"
