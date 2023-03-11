13250bbe61020a96da7c51af19aadc2b0d675478
Milan Suk <Milansuk@email.cz>
2023-03-11 16:42:14 +0100

    refactor: simplify js api

--- example-app/api.ts
+++ example-app/api.ts
@@ -5,21 +5,12 @@ import { Logger } from "./logging.js";
 export const createApi = (logger: Logger) => {
   const register = new Registry();
 
-  try {
-    collectDefaultMetrics({
-      register,
-      labels: { app: "test-api" },
-    });
-  } catch (e) {
-    logger.error(`Metrics setup error ${e}`);
-  }
+  collectDefaultMetrics({
+    register,
+    labels: { app: "test-api" },
+  });
 
   return createServer(async (req, res) => {
-    if (req.url === undefined) {
-      logger.error("Unexpected url");
-      throw new Error("No url");
-    }
-
     if (req.url === "/metrics") {
       logger.info("Handling /metrics");
       res.write(await register.metrics());
